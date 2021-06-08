35521e99d2a5e796fb48de16d2f0d1730f9e12bf
al <alfredo.yap@stu.bmcc.cuny.edu>
2021-06-07 20:00:28 -0400

    Modified the files index.html, script.js, and style.css

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+	var principal = document.getElementById("principal").value;
+	var rate = document.getElementById("rate").value;
+	var years = document.getElementById("years").value;
+	var interest = principal * years * (rate / 100);
+	var year = new Date().getFullYear() + parseInt(years);
+	var result = document.getElementById("result");
+
+	if (principal <=0){
+		alert("Enter a positive number");
+		document.getElementById("principal").focus();
+	}
+	else {
+		result.innerHTML = "<p>If you deposit <b><mark>"+principal+"<\/mark><\/b>,<\/p>";
+		result.innerHTML += "<p>at an interest rate of <b><mark>"+rate+"%<\/mark><\/b>.<\/p>";
+		result.innerHTML += "<p>You will receive an amount of <b><mark>"+interest+"<\/mark><\/b>,<\/p>";
+		result.innerHTML += "<p>in the year <b><mark>"+year;+"<\/mark><\/b><\/p>";
+	}
+	
 }
-        
+
+function updateRate()
+{
+	var rateval = document.getElementById("rate").value;
+	document.getElementById("rate_val").innerText = rateval+"%";
+}
+
